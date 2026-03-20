51c3607fff34032db1499872bbdf8b1bd77d0c05
nahom2251 <nahomalehegne@gmail.com>
2026-03-20 12:31:20 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onOfflineReady() {
+    console.log("App is ready for offline use");
+  },
+  onNeedRefresh() {
+    console.log("New update available");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
