343be4422542def8eb90efcbd38af62d03e2f835
nahom2251 <nahomalehegne@gmail.com>
2026-03-20 12:54:00 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { registerSW } from "virtual:pwa-register";
 
-registerSW({
+const updateSW = registerSW({
   onOfflineReady() {
     console.log("App is ready for offline use");
   },
   onNeedRefresh() {
     console.log("New update available");
+
+    // optional auto-refresh or manual prompt
+    if (confirm("New update available. Reload now?")) {
+      updateSW(true);
+    }
   },
 });
 
